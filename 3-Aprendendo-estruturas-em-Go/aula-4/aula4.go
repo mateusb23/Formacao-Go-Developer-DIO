d5053ab36a7804375124e0d8855e8afaa1ce4695
mateusb23 <mateusb.bispo@gmail.com>
2025-05-06 22:49:03 -0300

    aula 4 --> Sobre Switch  e  aula 5 --> Sobre operadores lógicos condicionais com if

---
+++ 3-Aprendendo-estruturas-em-Go/aula-4/aula4.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i <= 10; i++ {
+
+		switch i {
+		case 0:
+			fmt.Println("zero")
+		case 1:
+			fmt.Println("um")
+		case 2:
+			fmt.Println("dois")
+		case 3:
+			fmt.Println("três")
+		case 4:
+			fmt.Println("quatro")
+		case 5:
+			fmt.Println("cinco")
+		case 6:
+			fmt.Println("seis")
+		case 7:
+			fmt.Println("sete")
+		case 8:
+			fmt.Println("oito")
+		case 9:
+			fmt.Println("nove")
+		case 10:
+			fmt.Println("dez")
+		}
+	}
+}
